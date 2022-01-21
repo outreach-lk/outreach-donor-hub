56e471a559b9c333300899793e1c9043d1ce0fb2
Shehan Kulathilake <shehanhere@gmail.com>
2022-01-22 00:45:12 +0530

    moved auth & storage client factory specs to one unified spec for base. github workflow changed

--- app/apis/BaseClientFactory.ts
+++ app/apis/BaseClientFactory.ts
@@ -4,8 +4,8 @@
  */
 export default abstract class BaseClientFactory<T,P> {
     private _client:T | null= null;
-    protected abstract createClient(provider:P): T;
-    public getClient(provider:P){
+    protected abstract createClient(provider?:P): T;
+    public getClient(provider?:P){
         if(this._client === null){
             this._client = this.createClient(provider);
         }
---
+++ app/apis/base.client.factory.mock.ts
@@ -0,0 +1,29 @@
+import BaseClientFactory from "./BaseClientFactory";
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface IMockClient {
+    provider: MockProvider
+}
+export enum MockProvider {
+    PROVIDER1,
+    PROVIDER2,
+}
+
+class Provider1MockClient implements IMockClient{
+    provider: MockProvider = MockProvider.PROVIDER1;
+}
+class Provider2MockClient implements IMockClient{
+    provider: MockProvider = MockProvider.PROVIDER2;
+}
+
+export default class MockClientFactory extends BaseClientFactory<IMockClient, MockProvider>{
+    protected createClient(provider?: MockProvider): IMockClient {
+        switch(provider){
+            default:
+            case MockProvider.PROVIDER1:
+                return new Provider1MockClient();
+            case MockProvider.PROVIDER2:
+                return new Provider2MockClient();
+        }
+    }
+
+}
