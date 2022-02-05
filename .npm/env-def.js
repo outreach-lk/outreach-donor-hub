cc2680aac0d12b24aea81308dd0a30c9234513d3
Shehan Kulathilake <shehanhere@gmail.com>
2022-02-05 16:03:21 +0530

    generate env script

---
+++ .npm/env-def.js
@@ -0,0 +1,15 @@
+const envDef = {
+  /** server private key for all services */
+  SERVER_PRIVATE_KEY: null,
+
+  /** Google Service account secrets for firebase admin sdk */
+  GOOGLE_SERVICE_ACCT_EMAIL: null,
+  GOOGLE_SERVICE_ACCT_PK: null,
+
+  /** Public Environment Variables */
+  /** firebase configuration */
+  NEXT_PUBLIC_FIREBASE_PROJ_ID: null,
+  NEXT_PUBLIC_FIREBASE_STORAGE: null,
+};
+
+module.exports = envDef;
