fec99dc88ebaabe68bff4180844db0859c64c749
Shehan Kulathilake <shehanhere@gmail.com>
2022-02-26 05:03:16 +0530

    added firebase-admin alias set to false in webpack conf

--- next.config.js
+++ next.config.js
@@ -5,7 +5,23 @@ const nextConfig = {
   reactStrictMode: true,
   eslint: {
     dirs: ["app", "pages"],
-  }
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+        // set alias of node specific modules to false
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            'firebase-admin': false,
+        }
+    } else {
+      // set alias of browser only modules to false.
+        config.resolve.alias = {
+          ...config.resolve.alias,
+        }
+    }
+
+    return config;
+}
 };
 
 module.exports = nextConfig;
